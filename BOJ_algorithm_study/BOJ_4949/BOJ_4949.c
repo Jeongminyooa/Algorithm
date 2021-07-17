e8ad4617b4f527edcc452a361501c82938e4f543
Jeongminyooa <jmjmjmz732002@naver.com>
2021-07-17 20:51:33 +0900

    boj_4949 solved

---
+++ BOJ_algorithm_study/BOJ_4949/BOJ_4949.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZE 100
+char arr[SIZE];
+int top = -1;
+
+void init() {
+    top = -1;
+}
+
+int push(char x)
+{
+    if (top >= SIZE - 1) return -1;
+    return arr[++top] = x;
+    
+}
+
+int pop()
+{
+    if (top < 0) return -1;
+    else return arr[top--] = '\0';
+}
+
+char peek() {
+    if (top >= SIZE -1 || top < 0)
+        return -1;
+
+    return arr[top];
+}
+int empty() {
+    if (top == -1) return 1;
+    else return -1;
+}
+int main(void) {
+    char input[SIZE];
+
+    gets(input);
+
+    while (strcmp(input, ".") != 0) {
+        init();
+
+        for (int i = 0; input[i] != '\0'; i++) {
+            if(input[i] == '(' || input[i] == ')' || input[i] == '[' || input[i] == ']') {
+                if (peek() == '(' && input[i] == ')') pop();
+                else if (peek() == '[' && input[i] == ']') pop();
+                else
+                    push(input[i]);
+            }
+        }
+        if (empty() == 1) 
+            printf("yes\n");
+        else 
+            printf("no\n");
+
+        gets(input);
+    }
+    return 0;
+}
