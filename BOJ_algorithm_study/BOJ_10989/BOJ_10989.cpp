6b043a4e967c61e2e32c5442131a5d433b93effe
Jeongminyooa <jmjmjmz732002@naver.com>
2021-07-18 19:19:12 +0900

    BOJ_sort solved

---
+++ BOJ_algorithm_study/BOJ_10989/BOJ_10989.cpp
@@ -0,0 +1,28 @@
+//Counting Sort를 사용해서 문제 해결
+#include <stdio.h>
+
+#define SIZE 10001
+int cnt[SIZE];
+
+int main()
+{
+	int n;
+	int idx; // count에 쓰일 인덱스
+	long long i;
+	int j;
+
+	scanf_s("%d", &n);
+	
+	//n의 범위가 0 <= N <= 10,000,000이므로 long long 사용
+	for (i = 0; i < n; i++) {
+		scanf_s("%d", &idx);
+		cnt[idx]++;
+	}
+
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < cnt[i]; j++) {
+			printf("%d\n", i);
+		}
+	}
+	
+}
